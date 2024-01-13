eb3d902d8bd2fec34ebd2f3b5f28991efaa065f9
Oleg007git <cusniroleg@icloud.com>
2024-01-12 21:54:24 -0800

    111

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+let quantity = 3;
+
+let pricePerDroid = 3000;
+
+function makeTransaction(quantity, pricePerDroid) {
+  let = totalPrice;
+  let totalPrice = quantity * pricePerDroid;
+}
+
+const makeTransaction =
+  "You ordered" + quantity + " droids worth " + totalPrice + " credits!";
+
+console.log(makeTransaction);
+//  !
